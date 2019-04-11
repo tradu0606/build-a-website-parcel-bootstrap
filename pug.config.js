347a8c7e86d295e46e964bd74697829a0dc88164
Tetyana Radu <tanyaradu@Tanyas-MBP.home>
2019-04-10 22:41:52 -0400

    some progress

---
+++ pug.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    locals: {
+        imgSrc:["../img_carousel/200w (1).webp","../img_carousel/200w (2).webp","../img_carousel/200w (3).webp","../img_carousel/200w (4).webp","../img_carousel/200w (5).webp","../img_carousel/200w (6).webp","../img_carousel/200w (7).webp","../img_carousel/200w (8).webp","../img_carousel/200w (9).webp","../img_carousel/200w (10).webp","../img_carousel/200w (11).webp","../img_carousel/200w (12).webp","../img_carousel/200w (13).webp","../img_carousel/200w (14).webp","../img_carousel/200w (15).webp","../img_carousel/200w (16).webp", "../img_carousel/200w (17).webp"],
+    }
+}

200a56cf370b23488d3973e599bb55066cfeec8c
Tetyana Radu <tanyaradu@Tanyas-MacBook-Pro.local>
2019-04-11 12:12:35 -0400

    content added

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 console.log('test')
+
+function todayDay(){
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    
+    var d = new Date();
+    var i = d.getDate();
+
+    document.querySelector(".todayDay").innerHTML = `Today  ${months[d.getMonth()]} ${i}`
+}
+todayDay()
